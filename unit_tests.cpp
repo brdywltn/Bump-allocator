da20757f6ebafaf1d12c6c1ada6063b6229159d3
Brody <brody2.wilton@live.uwe.ac.uk>
2023-12-08 16:36:46 +0000

    add second unit test

--- unit_tests.cpp
+++ unit_tests.cpp
@@ -5,6 +5,10 @@ char const *groupAllocateAndDeallocate[] = {
     "BumpUp_AllocateAndDeallocate",
 };
 
+char const *groupAllocateToFullCapacity[] = {
+    "BumpUp_AllocateToFullCapacity"
+};
+
 DEFINE_TEST_G(AllocateDealllocateSizesTest, BumpUp_AllocateAndDeallocate) {
     //Test 1: Allocate and deallocate various sizes of mem
     BumpUpAllocator<int> allocator1(100);
@@ -13,11 +17,24 @@ DEFINE_TEST_G(AllocateDealllocateSizesTest, BumpUp_AllocateAndDeallocate) {
     allocator1.dealloc(x);
 }
 
+DEFINE_TEST_G(AllocateToFullCapacityTest, BumpUp_AllocateToFullCapacity) {
+    //Test 2: Allocate up to the full capacity of assigned mem
+    BumpUpAllocator<char> allocator2(10);
+    for (int i = 0; i < 3; i++){
+        char* block = allocator2.alloc(3);
+        TEST_MESSAGE(block != nullptr, "Memory allocation failed!");
+    }
+}
+
 int main() {
 
     bool pass = true; 
 
     for (auto group: groupAllocateAndDeallocate){
+        pass &= TestFixture::ExecuteTestGroup(group, TestFixture::Verbose);
+    }
+
+    for (auto group: groupAllocateToFullCapacity) {
         pass &= TestFixture::ExecuteTestGroup(group, TestFixture::Verbose);
     }
 
